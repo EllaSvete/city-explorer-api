038afb054ba12f78a29deeed82108ed7c9a5a46c
Ella Svete <ella.svete@gmail.com>
2022-03-22 17:10:46 -0700

    finished task 1 &2 back-end

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # city-explorer-api
+
+# Lab 7 city explorer-api
+
+**Author**: Ella Svete and Eden Brekke
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+
+This is an application used for searching cities in order to see their latitudes, longitudes, the name, and the weather.
+
+## Getting Started
+
+Install the necessary things in order to build your live server. 
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->
+
+## Credit and Collaborations
+
+Eden Brekke
+
+![Ella and Eden WRRC](/22March2022_WRRC_withElla.png)
+
+Name of feature: ________________________________
+
+Estimate of time needed to complete: __2hrs__
+
+Start time: __3:22___
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- server.js
+++ server.js
@@ -6,17 +6,48 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.envPORT || 3002;
+const weatherData = require('./data/weather.json');
 
 app.use(cors());
 
-app.get('/weather',weatherHandler);
+app.get('/weather', (request, response) => {
+  try {
+    // response.send(console.log('weather'));
+    let city = request.query.city;
+    console.log(city);
+    let cityWeather = weatherData.find(location => location.city_name === city);
+    console.log(cityWeather);
+    let weatherDisplay = [];
+    cityWeather.data.forEach(date => {
+      let forecast = new Forecast(date);
+      weatherDisplay.push(forecast);
+    });
+    // response.send(cityWeather.map({weatherDisplay}));
+
+    response.send(weatherDisplay);
+
+  } catch (error){
+    next(error);
+  }
+});
 
 app.get('*', (request, response) => {
   response.send('This page does not exist');
 });
 
-function weatherHandler(request, response){
-  response.send(console.log('weather'));
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+
+class Forecast {
+  constructor(element){
+    this.date = element.datetime;
+    this.description = element.weather.description;
+  }
 }
+
+
+
 
 app.listen(PORT,() => console.log(`listening on port ${PORT}`));
